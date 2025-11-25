614c6cfc6f3c0293233b9e2060cdbba9bf225212
arara0250 <arara4281@gmail.com>
2025-11-25 18:21:25 +0900

    feat : Item.cs 클래스 (not MonoBehaviour) 작성

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,16 @@
+public class Item
+{
+    /// <summary>
+    /// 아이템 동적 데이터 (아이템 객체별? 런타임에 변할 수 있는 정보들)
+    /// </summary>
+    public ItemData ItemData { get; private set; }
+    public int Quantity { get; private set; }
+    public bool IsEquipped { get; private set; }
+
+    public Item(ItemData data, int qty=1)
+    {
+        ItemData = data;
+        Quantity = qty;
+        IsEquipped = false;
+    }
+}
