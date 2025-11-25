68e7cf391b82555a3f968c5d552832cba8736403
arara0250 <arara4281@gmail.com>
2025-11-25 15:46:49 +0900

    feat : UIInventory.cs > 아이템 슬롯 동적 생성 로직 추가

--- Assets/Scripts/UIInventory.cs
+++ Assets/Scripts/UIInventory.cs
@@ -1,17 +1,33 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIInventory : MonoBehaviour
 {
     [SerializeField] private Button exitButton;
+    [SerializeField] private UISlot slotPrefab;
+    [SerializeField] private Transform slotParent;
+    [SerializeField] private int slotCount;
+
+    private List<UISlot> slots = new List<UISlot>();
 
     void Start()
     {
         exitButton.onClick.AddListener(CloseInventory);
+
+        InitInventoryUI();
     }
 
     private void CloseInventory()
     {
         UIManager.Instance.UIMainMenu.OpenMainMenu();
+    }
+
+    void InitInventoryUI()
+    {
+        for (int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab, slotParent);
+        }
     }
 }
