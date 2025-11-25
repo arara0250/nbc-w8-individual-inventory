f903d90b427ba7345aac540f5020912a359867d4
arara0250 <arara4281@gmail.com>
2025-11-25 12:15:29 +0900

    feat : Character.cs 클래스 (not MonoBehaviour) 작성
    필수 프로퍼티 및 생성자 작성

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
 
-public class Character : MonoBehaviour
+public class Character
 {
-    void Start()
-    {
-        
-    }
+    public string CharacterID { get; private set; }
+    public int Level { get; private set; }
+    public int Gold { get; private set; }
+    public int Health { get; private set; }
+    public int Attack { get; private set; }
+    public int Defense { get; private set; }
+    public int Speed { get; private set; }
 
-    void Update()
+    public Character(string id, int level, int gold, int hp, int att, int def, int spd)
     {
-        
+        CharacterID = id;
+        Level = level;
+        Gold = gold;
+        Health = hp;
+        Attack = att;
+        Defense = def;
+        Speed = spd;
     }
 }
