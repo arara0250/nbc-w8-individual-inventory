caad11216cb515566cb31ee35ebc8d467b9ef4a8
arara0250 <arara4281@gmail.com>
2025-11-25 12:20:21 +0900

    feat : GameManager.cs > 싱글톤 작성 및 SetData() 함수 추가
    SetData() : Player (Character 클래스) 생성 및 UI 세팅 함수 호출

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    void Start()
+    public Character Player { get; private set; }
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+
+        Instance = this;
     }
 
-    void Update()
+    private void Start()
     {
-        
+        SetData();
+    }
+
+    void SetData()
+    {
+        Player = new Character
+        (   
+            id: "arara",
+            level: 1,
+            gold: 500,
+            hp: 100,
+            att: 10,
+            def: 7,
+            spd: 5
+        );
+
+        UIManager.Instance.UIMainMenu.SetCharacterInfo(Player);
+        UIManager.Instance.UIStatus.SetStatusInfo(Player);
     }
 }
