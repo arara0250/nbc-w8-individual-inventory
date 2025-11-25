c2be7e28aa43544c578b1347ea44d4641a30d0ad
arara0250 <arara4281@gmail.com>
2025-11-25 15:44:14 +0900

    feat : UISlot.cs <-> UISlot 오브젝트 연결 및 프리팹화

---
+++ Assets/Scripts/UISlot.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISlot : MonoBehaviour
+{
+    [SerializeField] private Image itemIcon;
+    [SerializeField] private TextMeshProUGUI equipText;
+    [SerializeField] private Button slotButton;
+
+    void SetItem()
+    {
+
+    }
+
+    void RefreshUI()
+    {
+
+    }
+}
