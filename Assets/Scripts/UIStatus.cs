4f9c4a0bdd6e4617acdca551edf747285e873a3b
arara0250 <arara4281@gmail.com>
2025-11-26 14:27:30 +0900

    feat : UIStatus.cs > 장착 아이템 능력치 UI 반영 로직 추가

--- Assets/Scripts/UIStatus.cs
+++ Assets/Scripts/UIStatus.cs
@@ -4,11 +4,17 @@ using UnityEngine.UI;
 
 public class UIStatus : MonoBehaviour
 {
+    [Header("Buttons")]
     [SerializeField] private Button exitButton;
+
+    [Header("Info Texts")]
     [SerializeField] private TextMeshProUGUI healthText;
     [SerializeField] private TextMeshProUGUI attackText;
+    [SerializeField] private TextMeshProUGUI attAddText;
     [SerializeField] private TextMeshProUGUI defenseText;
+    [SerializeField] private TextMeshProUGUI defAddText;
     [SerializeField] private TextMeshProUGUI speedText;
+    [SerializeField] private TextMeshProUGUI spdAddText;
 
     void Start()
     {
@@ -22,9 +28,33 @@ public class UIStatus : MonoBehaviour
 
     public void SetStatusInfo(Character Player)
     {
+        attAddText.text = "";
+        defAddText.text = "";
+        spdAddText.text = "";
+
         healthText.text = Player.Health.ToString();
         attackText.text = Player.Attack.ToString();
         defenseText.text = Player.Defense.ToString();
         speedText.text = Player.Speed.ToString();
+
+        if (Player.EquippedItems.Count == 0) return;
+
+        foreach (Item item in Player.EquippedItems)
+        {
+            switch (item.ItemData.type)
+            {
+                case AbilityType.Attack:
+                    attAddText.text = $"(+{item.ItemData.amount})";
+                    break;
+                case AbilityType.Defense:
+                    defAddText.text = $"(+{item.ItemData.amount})";
+                    break;
+                case AbilityType.Speed:
+                    spdAddText.text = $"(+{item.ItemData.amount})";
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
